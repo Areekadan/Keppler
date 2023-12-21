556f53001cc670971e168ecb41cb0e7095d65091
Nick A <areekadann@mymacewan.ca>
2023-12-21 13:21:51 -0700

    Created Custom User Model
    Base User, Super User, etc. are now able to be created, edited, and filtered.

---
+++ keppler-src/apps/users/managers.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _ 
+
+class CustomUserManager(BaseUserManager):
+
+    def email_validator(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(_('Must provide a valid email address.'))
+        
+    def create_user(self, user_name, first_name, last_name, email, password, **extra_fields):
+        if not user_name:
+            raise ValueError(_('Username must be provided.'))
+        
+        if not first_name:
+            raise ValueError(_('First name must be provided.'))
+        
+        if not last_name:
+            raise ValueError(_('Last name must be provided.'))
+        
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_('Email must be provided to create a user account.'))
+        
+        user = self.model(
+            user_name=user_name,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user.save(using=self._db)
+        return user
+
+    
+    def create_superuser(self, user_name, first_name, last_name, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superusers must be staff. (is_staff = True)"))
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superusers must be superusers. (is_superuser = True)"))
+        
+        if not password:
+            raise ValueError(_("Password must be provided for an admin account."))
+        
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_('Email must be provided to create an admin account.'))
+        
+        user = self.create_user(user_name, first_name, last_name, email, password, **extra_fields)
+        user.save(using=self._db)
+        return user
+        
+        
