48c698ea2411d2239f4c4c0e3585d22298bf8b82
Nick A <areekadann@mymacewan.ca>
2023-12-21 17:44:35 -0700

    formatted document using Black Formatter
    Easier to read the models.py files.

--- keppler-src/apps/ratings/models.py
+++ keppler-src/apps/ratings/models.py
@@ -4,20 +4,34 @@ from keppler.settings.base import AUTH_USER_MODEL
 from apps.common.models import TimeStampedUUIDModel
 from apps.profiles.models import Profile
 
-class Rating(TimeStampedUUIDModel):
 
+class Rating(TimeStampedUUIDModel):
     class Range(models.IntegerChoices):
         Rating_1 = 1, _("Poor")
         Rating_2 = 2, _("Fair")
         Rating_3 = 3, _("Good")
         Rating_4 = 4, _("Very Good")
         Rating_5 = 5, _("Excellent")
-    
-    reviewer = models.ForeignKey(AUTH_USER_MODEL, verbose_name=_("Reviewer"), on_delete=models.SET_NULL, null=True)
-    seller = models.ForeignKey(Profile, verbose_name=_("Rated seller"), 
-        related_name="seller_review", on_delete=models.SET_NULL, null=True)
-    rating = models.IntegerField(verbose_name=_("Rating"), choices=Range.choices,
-        help_text="Select a rating: 1=Poor, 2=Fair, 3=Good, 4=Very Good, 5=Excellent. Choose a number that best reflects your evaluation.", default=0)
+
+    reviewer = models.ForeignKey(
+        AUTH_USER_MODEL,
+        verbose_name=_("Reviewer"),
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    seller = models.ForeignKey(
+        Profile,
+        verbose_name=_("Rated seller"),
+        related_name="seller_review",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    rating = models.IntegerField(
+        verbose_name=_("Rating"),
+        choices=Range.choices,
+        help_text="Select a rating: 1=Poor, 2=Fair, 3=Good, 4=Very Good, 5=Excellent. Choose a number that best reflects your evaluation.",
+        default=0,
+    )
     comment = models.TextField(verbose_name=_("Comment"))
 
     class Meta:
@@ -29,4 +43,3 @@ class Rating(TimeStampedUUIDModel):
             return f"Rating by {self.reviewer} for {self.seller}: {rating_label}"
         else:
             return f"Rating by {self.reviewer} for {self.seller}"
-    
--- keppler-src/apps/ratings/serializers.py
+++ keppler-src/apps/ratings/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Rating
 
+
 class RatingSerializer(serializers.ModelSerializer):
     reviewer = serializers.SerializerMethodField(read_only=True)
     seller = serializers.SerializerMethodField(read_only=True)
@@ -8,9 +9,9 @@ class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         exclude = ["updated_at", "pkid"]
-    
+
     def get_reviewer(self, obj):
         return obj.reviewer.username
-    
+
     def get_seller(self, obj):
         return obj.seller.user.username
