007177775c2b393ef9949d7461d74a324c99c4b5
Nick A <areekadann@mymacewan.ca>
2024-01-03 20:56:51 -0700

    Product Model Created
    - Added complete Product model in models.py.
    - Integrated AutoSlugField for automatic slug generation based on product title.
    - Included multiple image fields (cover_photo, photo1 to photo4) to accommodate up to five images per product.
    - Established foreign key relationship with User model for product ownership.
    - Defined custom model managers for published product filtering.
    - Implemented custom ProductStatus, AdvertType, and ProductType classes for structured choice fields.
    - Incorporated fields for product details such as price, quantity, dimensions, weight, tax, SKU, and ratings.
    - Added a ProductViews model to track views per product.
    - Prepared for future scalability with commented sections for a related ProductImage model.
    - Ensured all string fields are properly titled and descriptions capitalized in save method.
    - Added a property for calculating final product price including tax.
    - Set verbose names and plural names for models and fields where applicable.

---
+++ keppler-src/apps/products/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product, ProductViews
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "country", "advert_type", "product_type", "product_status"]
+    list_filter = ["country", "advert_type", "product_type", "product_status"]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductViews)
