e00cfc1087479138d12946ba0ac2030ba3e52c9c
Nick A <areekadann@mymacewan.ca>
2024-01-26 12:02:57 -0700

    NGINX setup with react and docker
    setup a react docker container as well NGINX is serving react and django on same port 8080.

---
+++ keppler-src/client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Keppler</h1>
+      <p>The Real Globalized Effort Begins...</p>
+    </div>
+  );
+}
+
+export default App;
